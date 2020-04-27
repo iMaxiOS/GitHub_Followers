55802b3cb33505b35d9a442820589a682c467406
Macostik <y.granchenko@gmail.com>
2020-04-27 21:32:02 +0300

    add searchVC and FavoritesVC

---
+++ GitHub_Followers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GitHub_Followers
+//
+//  Created by Maxim Granchenko on 27.04.2020.
+//  Copyright © 2020 Maxim Granchenko. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
--- GitHub_Followers/SearchVC.swift
+++ GitHub_Followers/SearchVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GitHub_Followers
 //
 //  Created by Maxim Granchenko on 27.04.2020.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
     }
-
-
 }
-
