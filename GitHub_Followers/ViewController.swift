c35966031072bd2814e7ab4ae6439e4cef2f4f4b
Macostik <y.granchenko@gmail.com>
2020-04-27 20:53:31 +0300

    add project

---
+++ GitHub_Followers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub_Followers
+//
+//  Created by Maxim Granchenko on 27.04.2020.
+//  Copyright © 2020 Maxim Granchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
