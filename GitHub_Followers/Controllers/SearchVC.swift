2c11f43d1b8306669a00eb71c60be01c28844871
Macostik <y.granchenko@gmail.com>
2020-04-28 21:26:26 +0300

    implementation search scene

--- GitHub_Followers/Controllers/SearchVC.swift
+++ GitHub_Followers/Controllers/SearchVC.swift
@@ -9,14 +9,50 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    private let imageView = UIImageView(image: #imageLiteral(resourceName: "gh-logo"))
+    private let nameTextField = GFTextField()
+    private let getButton = GFButton(bgColor: .systemGreen, title: "Get Followers")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
+        
+        configure()
+        addConstraints()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = true
     }
+    
+    private func configure() {
+        view.addSubview(imageView)
+        view.addSubview(nameTextField)
+        view.addSubview(getButton)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: view.frame.width * 0.5),
+            imageView.heightAnchor.constraint(equalToConstant: view.frame.width * 0.5),
+            
+            nameTextField.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 60),
+            nameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            nameTextField.heightAnchor.constraint(equalToConstant: 50),
+            
+            getButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            getButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            getButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            getButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    
 }
