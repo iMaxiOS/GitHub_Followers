0293c57330cc6aac914f1c8f2a2e6a69f4d7785c
Macostik <y.granchenko@gmail.com>
2020-05-05 12:00:45 +0300

    dismiss activity animation

--- GitHub_Followers/Extensions/UIViewController+Ext.swift
+++ GitHub_Followers/Extensions/UIViewController+Ext.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
     
     public func presentGFAlertOnMain(title: String, body: String, titleButton: String) {
@@ -16,6 +18,31 @@ extension UIViewController {
             alertVC.modalPresentationStyle = .overFullScreen
             alertVC.modalTransitionStyle = .crossDissolve
             self.present(alertVC, animated: true, completion: nil)
+        }
+    }
+    
+    public func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            containerView.alpha = 0.8
+        }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.center = containerView.center
+        activityIndicator.startAnimating()
+    }
+    
+    public func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
